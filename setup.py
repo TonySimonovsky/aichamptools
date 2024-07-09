2bc283ba55684a811cd6e04007a867df4f85040d
TonySimonovsky <metton@gmail.com>
2024-07-09 15:39:38 +0800

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aichamptools',
-    version='0.2.0',
+    version='0.2.1',
     description='A library for LLMOps including connectors to various LLMs and assessment tools',
     author='Tony AI Champ',
     author_email='tony@aicha.mp',
